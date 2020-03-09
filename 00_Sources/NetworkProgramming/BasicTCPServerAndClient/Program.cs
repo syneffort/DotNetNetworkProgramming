552476f1364203a9b22545e5964627acfa0a2331
Pyunghwa Kim <syneffort@gmail.com>
2020-03-10 07:24:36 +0900

    Basic TCP Server and Client

---
+++ 00_Sources/NetworkProgramming/BasicTCPServerAndClient/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace BasicTCPServerAndClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Server = 1, Client = 2 ****");
+            Console.Write("Input mode key : ");
+            string mode = Console.ReadLine();
+
+            string input;
+            switch (mode)
+            {
+                case "1":
+                    {
+                        Console.Write("IP : ");
+                        input = Console.ReadLine();
+                        IPAddress IP = IPAddress.Parse(input);
+
+                        TcpListener tcpListener = new TcpListener(IP, 7);
+                        tcpListener.Start();
+                        Console.WriteLine("Waiting...");
+                        TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                        NetworkStream ns = tcpClient.GetStream();
+                        byte[] ReceiveMessage = new byte[100];
+                        ns.Read(ReceiveMessage, 0, 100);
+                        string strMessage = Encoding.ASCII.GetString(ReceiveMessage);
+                        Console.WriteLine(strMessage);
+
+                        string EchoMessage = "Hi~~";
+                        byte[] SendMessage = Encoding.ASCII.GetBytes(EchoMessage);
+                        ns.Write(SendMessage, 0, SendMessage.Length);
+                        ns.Close();
+                        tcpClient.Close();
+                        tcpListener.Stop();
+
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.Write("IP : ");
+                        input = Console.ReadLine();
+
+                        TcpClient tcpClient = new TcpClient(input, 7);
+                        if (tcpClient.Connected)
+                        {
+                            Console.WriteLine("Sucess to connect server");
+                            NetworkStream ns = tcpClient.GetStream();
+                            string Message = "Hello World";
+                            byte[] SendByteMessage = Encoding.ASCII.GetBytes(Message);
+                            ns.Write(SendByteMessage, 0, SendByteMessage.Length);
+
+                            byte[] ReceiveByteMessage = new byte[32];
+                            ns.Read(ReceiveByteMessage, 0, 32);
+                            string ReceiveMessage = Encoding.ASCII.GetString(ReceiveByteMessage);
+                            Console.WriteLine(ReceiveMessage);
+                            ns.Close();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fail to connect server");
+                        }
+
+                        tcpClient.Close();
+
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Unknown mode");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- 00_Sources/NetworkProgramming/Intro/Program.cs
+++ 00_Sources/NetworkProgramming/Intro/Program.cs
@@ -12,16 +12,65 @@ namespace Intro
     {
         static void Main(string[] args)
         {
-            Console.Write("IP : ");
-            string Address = Console.ReadLine();
-            IPAddress IP = IPAddress.Parse(Address);
+            Console.Write("Input mode key : ");
+            string mode = Console.ReadLine();
 
-            Console.WriteLine("IP : {0}", IP.ToString());
-
-            IPAddress[] IPs = Dns.GetHostAddresses("www.google.com");
-            foreach (IPAddress HostIP in IPs)
+            string input;
+            switch (mode)
             {
-                Console.WriteLine("IP : {0}", HostIP);
+                // IP parsing
+                case "1":
+                    {
+                        Console.Write("IP : ");
+                        input = Console.ReadLine();
+                        IPAddress IP = IPAddress.Parse(input);
+
+                        Console.WriteLine("IP : {0}", IP.ToString());
+                        break;
+                    }
+                // Get host addresses (ip + domain)
+                case "2":
+                    {
+                        Console.Write("DNS : ");
+                        input = Console.ReadLine();
+                        IPAddress[] IPs = Dns.GetHostAddresses(input);
+
+                        foreach (IPAddress hostIP in IPs)
+                        {
+                            Console.WriteLine("IP : {0}", hostIP);
+                        }
+                        break;
+                    }
+                // Get host entry (ip + hostname)
+                case "3":
+                    {
+                        Console.Write("DNS : ");
+                        input = Console.ReadLine();
+                        IPHostEntry HostInfo = Dns.GetHostEntry(input);
+
+                        foreach (IPAddress hostIP in HostInfo.AddressList)
+                        {
+                            Console.WriteLine("IP : {0}", hostIP);
+                        }
+                        Console.WriteLine("HostName : {0}", HostInfo.HostName);
+                        break;
+                    }
+                // Set ip end point (ip + port)
+                case "4":
+                    {
+                        Console.Write("IP : ");
+                        input = Console.ReadLine();
+                        IPAddress IPInfo = IPAddress.Parse(input);
+                        int port = 13;
+                        IPEndPoint EndPointInfo = new IPEndPoint(IPInfo, port);
+
+                        Console.WriteLine("IP : {0} Port : {1}", EndPointInfo.Address, EndPointInfo.Port);
+                        Console.WriteLine(EndPointInfo.ToString());
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Unknown mode");
+                    break;
             }
 
             Console.ReadKey();
