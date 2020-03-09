1d91a4c3a61d6b03bf0c14b71fc58ebcdd544670
Pyunghwa Kim <syneffort@gmail.com>
2020-03-09 21:12:04 +0900

    Intro

--- 00_Sources/NetworkProgramming/Intro/Program.cs
+++ 00_Sources/NetworkProgramming/Intro/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+
 namespace Intro
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("IP : ");
+            string Address = Console.ReadLine();
+            IPAddress IP = IPAddress.Parse(Address);
+
+            Console.WriteLine("IP : {0}", IP.ToString());
+
+            IPAddress[] IPs = Dns.GetHostAddresses("www.google.com");
+            foreach (IPAddress HostIP in IPs)
+            {
+                Console.WriteLine("IP : {0}", HostIP);
+            }
+
+            Console.ReadKey();
         }
     }
 }
