2876504fc971a5c54fa0dd6102aa474bcf41186d
Pyunghwa Kim <syneffort@gmail.com>
2020-03-11 08:24:45 +0900

    To Winform 1/3

---
+++ 00_Sources/NetworkProgramming/BasicSeerver/MainWindow.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace BasicSeerver
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.btnConnnect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClientIP = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server IP :";
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Location = new System.Drawing.Point(95, 11);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(169, 21);
+            this.txtServerIP.TabIndex = 1;
+            // 
+            // btnConnnect
+            // 
+            this.btnConnnect.Location = new System.Drawing.Point(285, 10);
+            this.btnConnnect.Name = "btnConnnect";
+            this.btnConnnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnnect.TabIndex = 2;
+            this.btnConnnect.Text = "Connect";
+            this.btnConnnect.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Client IP :";
+            // 
+            // txtClientIP
+            // 
+            this.txtClientIP.Location = new System.Drawing.Point(95, 38);
+            this.txtClientIP.Name = "txtClientIP";
+            this.txtClientIP.ReadOnly = true;
+            this.txtClientIP.Size = new System.Drawing.Size(169, 21);
+            this.txtClientIP.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 65);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(348, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Send/Receive Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(372, 99);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnConnnect);
+            this.Controls.Add(this.txtClientIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServerIP);
+            this.Controls.Add(this.label1);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServerIP;
+        private System.Windows.Forms.Button btnConnnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClientIP;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ 00_Sources/NetworkProgramming/BasicSeerver/MainWindow.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BasicSeerver
+{
+    public partial class MainWindow : Form
+    {
+        TcpListener tcpListener;
+        TcpClient tcpClient;
+        NetworkStream ns;
+        BinaryWriter bw;
+        BinaryReader br;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitInstance();
+        }
+
+        private void InitInstance()
+        {
+            tcpListener = new TcpListener(IPAddress.Any, 3000);
+            tcpListener.Start();
+
+            this.Load += Form1_Load;
+            this.btnConnnect.Click += btnConnnect_Click;
+            this.btnStart.Click += btnStart_Click;
+        }
+
+        void btnStart_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        void btnConnnect_Click(object sender, EventArgs e)
+        {
+            tcpClient = tcpListener.AcceptTcpClient();
+            if (tcpClient.Connected)
+                txtClientIP.Text = ((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address.ToString();
+
+            ns = tcpClient.GetStream();
+            bw = new BinaryWriter(ns);
+            br = new BinaryReader(ns);
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            for (int i = 0 ; i < host.AddressList.Length; i++)
+            {
+                if (host.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    txtServerIP.Text = host.AddressList[i].ToString();
+                    break;
+                }
+            }
+        }
+    }
+}
