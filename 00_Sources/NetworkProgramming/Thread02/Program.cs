36e1857ae61686c32877179c85aa36f5cc47be26
Pyunghwa Kim <syneffort@gmail.com>
2020-03-13 11:36:53 +0900

    ommited files

---
+++ 00_Sources/NetworkProgramming/Thread02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread02
+{
+    class Program
+    {
+        class Test
+        {
+            public void Print()
+            {
+                Console.WriteLine("Test print");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            Thread th = new Thread(new ThreadStart(test.Print));
+            th.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
